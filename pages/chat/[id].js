1f903e99323567753727d70e4e020d9662738c1b
Danny <dannyaguirre09@hotmail.com>
2021-07-04 18:09:07 -0500

    commit

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import Head from 'next/head';
-import SideBar from '../../components/SideBar'
+import SideBar from '../../components/Sidebar'
 import ChatScreen from '../../components/ChatScreen'
 import {auth, db} from '../../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth';
